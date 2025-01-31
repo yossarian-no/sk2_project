1cb83d77ca44e09fb3358e69962f574e327933ab
Vasil Kusmartsev <145450951+yossarian-no@users.noreply.github.com>
2025-01-31 15:27:01 +0100

    Update telnet_client.py

--- telnet_client.py
+++ telnet_client.py
@@ -1,20 +1,32 @@
 import socket
 
-SERVER_IP = "127.0.0.1"  # wskaz adres servera
+
+SERVER_IP = "192.168.65.229"  
 PORT = 12345
-BUFFER_SIZE = 1024
+BUFFER_SIZE = 4096
 
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket.connect((SERVER_IP, PORT))
-
-print("Connected to server. Type commands to execute or 'exit' to quit.")
+try:
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect((SERVER_IP, PORT))
+    print("Connected to server. Type commands to execute or 'exit' to quit.")
+except Exception as e:
+    print(f"Connection failed: {e}")
+    exit(1)
 
 while True:
-    command = input("> ")
-    client_socket.send(command.encode())
-    if command.lower() == "exit":
+    try:
+        command = input("> ")
+        client_socket.send(command.encode())  
+
+        
+        if command.lower() == "exit":
+            break
+
+        
+        response = client_socket.recv(BUFFER_SIZE).decode()
+        print(response)
+    except Exception as e:
+        print(f"An error occurred: {e}")
         break
-    response = client_socket.recv(BUFFER_SIZE).decode()
-    print(response)
 
 client_socket.close()
