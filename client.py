633a70045a91a894f3bc05fb481bc737415d5d0d
Vasil Kusmartsev <145450951+yossarian-no@users.noreply.github.com>
2025-01-31 10:51:33 +0100

    Update client.py

--- client.py
+++ client.py
@@ -1,1 +1,20 @@
+import socket
 
+SERVER_IP = "127.0.0.1"  # wskaz adres servera
+PORT = 12345
+BUFFER_SIZE = 1024
+
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect((SERVER_IP, PORT))
+
+print("Connected to server. Type commands to execute or 'exit' to quit.")
+
+while True:
+    command = input("> ")
+    client_socket.send(command.encode())
+    if command.lower() == "exit":
+        break
+    response = client_socket.recv(BUFFER_SIZE).decode()
+    print(response)
+
+client_socket.close()
